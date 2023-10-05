be4741f59fce73e05523730de7d33b8088bf38dc
Emily Kelt <85489688+emilykelt@users.noreply.github.com>
2023-10-05 18:03:45 +0100

    Update radio.py

--- Cans/radio.py
+++ Cans/radio.py
@@ -3,9 +3,9 @@ import board
 import busio
 import adafruit_rfm9x
 
-spi = busio.SPI(clock=board.GP2, MOSI=board.GP3, MISO=board.GP4)
-cs = digitalio.DigitalInOut(board.GP6)
-reset = digitalio.DigitalInOut(board.GP7)
+spi = busio.SPI(clock=board.GP2, MOSI=board.GP3, MISO=board.GP4)    #this has the pins that are connected to the pico (and then wired to the radio)
+cs = digitalio.DigitalInOut(board.GP6)    #same here
+reset = digitalio.DigitalInOut(board.GP7)    #same here
 
 rfm9x = adafruit_rfm9x.RFM9x(spi, cs, reset, 433.0)
 
@@ -14,10 +14,10 @@ print("Radio Ready")
 def send(message):
     rfm9x.send(message)
 
-# groundstation, recieving:
+# groundstation, recieving: THIS CODE IS ONLY NECESSARY FOR THE GROUNDSTATION
 
 def try_read():
-    return rfm9x.recieve(timeout=1.0)
+    return rfm9x.recieve(timeout=1.0)    
 
 def rssi():
     return rfm9x.rssi
