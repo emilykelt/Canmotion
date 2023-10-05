dd755e15cc6ca5f972f27f3d303a016f2ddb3fab
Emily Kelt <85489688+emilykelt@users.noreply.github.com>
2023-10-05 18:02:02 +0100

    Update tmp36.py

--- Cans/tmp36.py
+++ Cans/tmp36.py
@@ -4,7 +4,9 @@ adc = analogio.AnalogIn(board.GP26)
 
 def read_temperature():
     adc_raw = adc.value
+    #converts analogue temperature to digital
+    
     adc_voltage = (adc_raw * 3.3)/65536
     temperature = (adc_voltage - 0.5) * 100
-    # print("raw = {:5d} volts = {:5.2f}".format(adc_raw, adc_voltage, temperature))
+
     return temperature
