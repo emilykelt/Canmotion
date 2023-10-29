95e0b475e4ba612aea07d58d55c0d327639a8d32
Amelia484 <138249982+Amelia484@users.noreply.github.com>
2023-10-29 10:09:04 +0000

    Update graphplotting.py
    Updated this program to use resultant variables for accelerations. Note that example.csv will not work anymore unless the variable names are changed back to X_Acceleration etc.

--- dataprocessing/graphplotting.py
+++ dataprocessing/graphplotting.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import io
-#As is, this code will produce three graphs, X_Acceleration against Time, as well Y_Acceleration and Z_Acceleration against Time.
+#As is, this code will produce three graphs, Resultant_X_Acceleration against Time, as well Resultant_Y_Acceleration and Resultant_Z_Acceleration against Time. This isn't on example.csv but using the resultant vector program should give the required data.
 #(Also creates a table but we'll have that anyways as it is the same as the .csv)
 #I don't know how to plot the three lines on the same graph, if anyone knows how to then feel free to do so. Also feel free to use any loops to make this more efficient. Can't be bothered doing it myself.
 #Could also do with fixing ymin and ymax to be the same for all three graphs, maybe will do later.
@@ -17,7 +17,7 @@ print(table_1.head())
 
 # Convert values to numpy arrays to graph
 x = table_1['Time'].to_numpy()
-y = table_1['X_Acceleration'].to_numpy()
+y = table_1['Resultant_X_Acceleration'].to_numpy()
 # Graph dimensions
 xmin = x.min()
 xmax = x.max()
@@ -30,7 +30,7 @@ plt.plot([xmin,xmax],[0,0],'b') # blue x axis
 plt.plot([0,0],[ymin,ymax], 'b') # blue y axis
 plt.plot(x,y) # make it scatterplot or line graph. (x,y,ro) for scattergraph
 
-y = table_1['Y_Acceleration'].to_numpy()
+y = table_1['Resultant_Y_Acceleration'].to_numpy()
 
 xmin = x.min()
 xmax = x.max()
@@ -43,7 +43,7 @@ plt.plot([xmin,xmax],[0,0],'b') # blue x axis
 plt.plot([0,0],[ymin,ymax], 'b') # blue y axis
 plt.plot(x,y,color='red') # make it scatterplot or line graph. (x,y,ro) for scattergraph
 
-y = table_1['Z_Acceleration'].to_numpy()
+y = table_1['Resultant_Z_Acceleration'].to_numpy()
 
 xmin = x.min()
 xmax = x.max()
